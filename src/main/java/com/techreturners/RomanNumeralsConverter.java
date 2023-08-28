d89b382e6457a1c0584219d94dac0948fb0bd68c
Swapna V <vswapna3202@gmail.com>
2023-08-28 19:14:04 +0100

    feat: Added try catch loop

--- src/main/java/com/techreturners/RomanNumeralsConverter.java
+++ src/main/java/com/techreturners/RomanNumeralsConverter.java
@@ -1,23 +1,28 @@
 package com.techreturners;
 
+import java.util.InputMismatchException;
+
 public class RomanNumeralsConverter {
 
     public Object convert(int decimalNumber) {
-
-        if (decimalNumber <= 0 || decimalNumber > 3999){
-            return "Invalid Input!";
-        }
-
-        String[] romanChars = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        int[] decimalValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-
-        StringBuilder romanNumber = new StringBuilder();
-        for (int i=0;i<decimalValues.length;i++){
-            while(decimalNumber >= decimalValues[i]) {
-                decimalNumber -= decimalValues[i];
-                romanNumber.append(romanChars[i]);
+        try {
+            if (decimalNumber <= 0 || decimalNumber > 3999) {
+                return "Invalid Input!";
             }
+
+            String[] romanChars = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+            int[] decimalValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+            StringBuilder romanNumber = new StringBuilder();
+            for (int i = 0; i < decimalValues.length; i++) {
+                while (decimalNumber >= decimalValues[i]) {
+                    decimalNumber -= decimalValues[i];
+                    romanNumber.append(romanChars[i]);
+                }
+            }
+            return romanNumber.toString();
+        }catch(InputMismatchException ime){
+            return("Invalid Input!");
         }
-        return romanNumber.toString();
     }
 }
