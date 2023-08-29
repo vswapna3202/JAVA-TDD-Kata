d1a1a26861e5c37b2fa81b5dc9ff4abe088122bd
Swapna V <vswapna3202@gmail.com>
2023-08-29 21:26:31 +0100

    feat: Added test case and code

--- src/main/java/com/techreturners/BowlingGame.java
+++ src/main/java/com/techreturners/BowlingGame.java
@@ -1,29 +1,14 @@
 package com.techreturners;
 
 public class BowlingGame {
-    private int[] bowlingScores = new int[20];
-    private int index = 0;
-
+    private int[] bowlingScores = new int[21];
+    int index = 0;
     public void rollPins(int score){
         bowlingScores[index++] = score;
     }
 
     public int calculateScore(){
-        int score = 0;
-        int frameLength = 10;
-        int scoreIndex = 0;
-        for (int i = 0; i < frameLength; i++){
-            if (bowlingScores[scoreIndex] + bowlingScores[scoreIndex + 1] == 10){
-                //this is a spare
-                score += 10 + bowlingScores [scoreIndex + 2];
-                scoreIndex += 2;
-            } else {
-                // this is a miss case
-                score += bowlingScores[scoreIndex] + bowlingScores[scoreIndex + 1];
-                scoreIndex += 2;
-            }
-        }
-        return (score);
+        return 0;
     }
 
 }
